54e2e5a6180893ebc99c9767afd605e33c1d3627
Hailey <carly1982@naver.com>
2023-07-15 16:06:53 +0900

    Second commit
    for color in colors:
        rgb_colors.append(color.rgb)
    
    print(rgb_colors)
    
    => result: Rgb(r=245, g=238, b=243)

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ for color in colors:
 
 
 print(rgb_colors)
+
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+
+# for color in colors: 
+#     rgb_colors.append(color.rgb)    
+
+# print(rgb_colors) # result: Rgb(r=245, g=238, b=243)
